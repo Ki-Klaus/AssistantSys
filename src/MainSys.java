951c077592f874d9d8430efbe9d81fe741218821
KKlaus <136526154@qq.com>
2017-04-26 19:08:37 +0800

    added codes modified

--- src/MainSys.java
+++ src/MainSys.java
@@ -1,12 +1,66 @@
 import javax.swing.JFrame;
 
+import com.iflytek.cloud.speech.RecognizerListener;
+import com.iflytek.cloud.speech.RecognizerResult;
+import com.iflytek.cloud.speech.SpeechConstant;
+import com.iflytek.cloud.speech.SpeechError;
+import com.iflytek.cloud.speech.SpeechRecognizer;
+import com.iflytek.cloud.speech.SpeechUtility;
+
 public class MainSys {
 
+	public static final String appid = "58fe1a81";
+
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("My Frame");
-		frame.setSize(400, 300);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		initIFly();
+		initParam();
 	}
+
+	private static void initIFly() {
+		SpeechUtility.createUtility(appid);
+	}
+
+	private static void initParam() {
+		SpeechRecognizer mIat = SpeechRecognizer.createRecognizer();
+		mIat.setParameter(SpeechConstant.LANGUAGE, "en_us");
+		mIat.startListening(mRecognizerListener);
+	}
+
+	private static RecognizerListener mRecognizerListener = new RecognizerListener() {
+
+		@Override
+		public void onVolumeChanged(int arg0) {
+			// TODO 自动生成的方法存根
+
+		}
+
+		@Override
+		public void onResult(RecognizerResult arg0, boolean arg1) {
+			// TODO 自动生成的方法存根
+
+		}
+
+		@Override
+		public void onEvent(int arg0, int arg1, int arg2, String arg3) {
+			// TODO 自动生成的方法存根
+
+		}
+
+		@Override
+		public void onError(SpeechError arg0) {
+			arg0.getErrorDescription(true);
+		}
+
+		@Override
+		public void onEndOfSpeech() {
+			// TODO 自动生成的方法存根
+
+		}
+
+		@Override
+		public void onBeginOfSpeech() {
+			// TODO 自动生成的方法存根
+
+		}
+	};
 }

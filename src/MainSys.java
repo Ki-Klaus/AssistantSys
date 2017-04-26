694f123bac94199f658b49235dbf82c256211404
KKlaus <136526154@qq.com>
2017-04-26 19:00:46 +0800

    added some basic iFly codes

--- src/MainSys.java
+++ src/MainSys.java
@@ -1,6 +1,12 @@
+import javax.swing.JFrame;
+
 public class MainSys {
 
 	public static void main(String[] args) {
-		System.out.println("123");
+		JFrame frame = new JFrame("My Frame");
+		frame.setSize(400, 300);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 }

405c5b480a9d9944c07b8214eff236003c015a5e
KKlaus <136526154@qq.com>
2017-05-03 18:32:41 +0800

    added robot

--- src/com/assistantsys/main/RobotUtil.java
+++ src/com/assistantsys/main/RobotUtil.java
@@ -39,16 +39,156 @@ public class RobotUtil {
 		}
 		return s;
 	}
+	
+	public void pressA(){
+		mRobot.keyPress(KeyEvent.VK_A);
+		mRobot.keyRelease(KeyEvent.VK_A);
+	}
+	
+	public void pressB(){
+		mRobot.keyPress(KeyEvent.VK_B);
+		mRobot.keyRelease(KeyEvent.VK_B);
+	}
+	
+	public void pressC(){
+		mRobot.keyPress(KeyEvent.VK_C);
+		mRobot.keyRelease(KeyEvent.VK_C);
+	}
+	
+	public void pressD(){
+		mRobot.keyPress(KeyEvent.VK_D);
+		mRobot.keyRelease(KeyEvent.VK_D);
+	}
+	
+	public void pressE(){
+		mRobot.keyPress(KeyEvent.VK_E);
+		mRobot.keyRelease(KeyEvent.VK_E);
+	}
+	
+	public void pressF(){
+		mRobot.keyPress(KeyEvent.VK_F);
+		mRobot.keyRelease(KeyEvent.VK_F);
+	}
+	
+	public void pressG(){
+		mRobot.keyPress(KeyEvent.VK_G);
+		mRobot.keyRelease(KeyEvent.VK_G);
+	}
+	
+	public void pressH(){
+		mRobot.keyPress(KeyEvent.VK_H);
+		mRobot.keyRelease(KeyEvent.VK_H);
+	}
+	
+	public void pressI(){
+		mRobot.keyPress(KeyEvent.VK_I);
+		mRobot.keyRelease(KeyEvent.VK_I);
+	}
 
+	public void pressJ(){
+		mRobot.keyPress(KeyEvent.VK_J);
+		mRobot.keyRelease(KeyEvent.VK_J);
+	}
+	
+	public void pressK(){
+		mRobot.keyPress(KeyEvent.VK_K);
+		mRobot.keyRelease(KeyEvent.VK_K);
+	}
+	
+	public void pressL(){
+		mRobot.keyPress(KeyEvent.VK_L);
+		mRobot.keyRelease(KeyEvent.VK_L);
+	}
+	
+	public void pressM(){
+		mRobot.keyPress(KeyEvent.VK_M);
+		mRobot.keyRelease(KeyEvent.VK_M);
+	}
+	
+	public void pressN(){
+		mRobot.keyPress(KeyEvent.VK_N);
+		mRobot.keyRelease(KeyEvent.VK_N);
+	}
+	
+	public void pressO(){
+		mRobot.keyPress(KeyEvent.VK_O);
+		mRobot.keyRelease(KeyEvent.VK_O);
+	}
+	
+	public void pressP(){
+		mRobot.keyPress(KeyEvent.VK_P);
+		mRobot.keyRelease(KeyEvent.VK_P);
+	}
+	
+	public void pressQ(){
+		mRobot.keyPress(KeyEvent.VK_Q);
+		mRobot.keyRelease(KeyEvent.VK_Q);
+	}
+	
+	public void pressR(){
+		mRobot.keyPress(KeyEvent.VK_R);
+		mRobot.keyRelease(KeyEvent.VK_R);
+	}
+	
+	public void pressS(){
+		mRobot.keyPress(KeyEvent.VK_S);
+		mRobot.keyRelease(KeyEvent.VK_S);
+	}
+	
+	public void pressT(){
+		mRobot.keyPress(KeyEvent.VK_T);
+		mRobot.keyRelease(KeyEvent.VK_T);
+	}
+	
+	public void pressU(){
+		mRobot.keyPress(KeyEvent.VK_U);
+		mRobot.keyRelease(KeyEvent.VK_U);
+	}
+	
+	public void pressV(){
+		mRobot.keyPress(KeyEvent.VK_V);
+		mRobot.keyRelease(KeyEvent.VK_V);
+	}
+	
+	public void pressW(){
+		mRobot.keyPress(KeyEvent.VK_W);
+		mRobot.keyRelease(KeyEvent.VK_W);
+	}
+	
+	public void pressX(){
+		mRobot.keyPress(KeyEvent.VK_X);
+		mRobot.keyRelease(KeyEvent.VK_X);
+	}
+	
+	public void pressY(){
+		mRobot.keyPress(KeyEvent.VK_Y);
+		mRobot.keyRelease(KeyEvent.VK_Y);
+	}
+	
+	public void pressZ(){
+		mRobot.keyPress(KeyEvent.VK_Z);
+		mRobot.keyRelease(KeyEvent.VK_Z);
+	}
+	
 	private StringBuffer robotCase(int position) {
 		switch (position) {
 		case 0:
-			mRobot.keyPress(KeyEvent.VK_A);
-			mRobot.keyRelease(KeyEvent.VK_A);
+			pressA();
 			break;
 		case 1:
-			mRobot.keyPress(KeyEvent.VK_B);
-			mRobot.keyRelease(KeyEvent.VK_B);
+			pressB();
+			break;
+		case 2:
+			pressC();
+			break;
+		case 3:
+			pressD();
+			break;
+		case 4:
+			pressD();
+			break;
+		case 5:
+			pressE();
 			break;
 		}
 		return null;
